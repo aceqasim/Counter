4e36eb5543b8fb7080e98c019500ef2de7db4043
aceqasim <m.kaxim21@gmail.com>
2025-07-16 23:27:49 +0500

    ussestate hook and useefect hook

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UseStateV from './components/hooks/UseStateV'
+import UseeffectHook from './components/hooks/UseeffectHook'
+
+const App = () => {
+  return (
+     <>
+     {/* <UseStateV/> */}
+     <UseeffectHook/>
+     </>
+  )
+}
+
+export default App
