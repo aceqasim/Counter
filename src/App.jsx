db5c39113e87a6a9a303a84fe3494893b8802852
aceqasim <m.kaxim21@gmail.com>
2025-07-16 23:45:11 +0500

    updated the code vith all hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import UseStateV from './components/hooks/UseStateV'
 import UseeffectHook from './components/hooks/UseeffectHook'
+import UseReducerHook from './components/hooks/UseReducerHook'
 
 const App = () => {
   return (
      <>
      {/* <UseStateV/> */}
-     <UseeffectHook/>
+     {/* <UseeffectHook/> */}
+     <UseReducerHook/>
      </>
   )
 }
